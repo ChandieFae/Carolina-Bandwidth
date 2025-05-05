820132292d91256786089743b42382f8ea8519a7
Chandra Brown <chandie29@gmail.com>
2025-05-05 02:42:20 -0400

    Update whisper_benchmark.py
    whisper_benchmark.py

--- whisper_benchmark.py
+++ whisper_benchmark.py
@@ -1,1 +1,9 @@
-whisper_benchmark.py
+## experiments/whisper_benchmark.py
+from time import time
+from models.whisper_loader import transcribe_audio
+
+start = time()
+result = transcribe_audio("sample.wav")
+end = time()
+print("Result:", result)
+print("Time taken:", end - start)
