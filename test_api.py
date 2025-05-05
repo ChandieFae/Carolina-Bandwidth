d4b72f4c82e260e720c42d6f26820d1b724e4147
Chandra Brown <chandie29@gmail.com>
2025-05-05 02:43:19 -0400

    Update test_api.py
    test_api.py

--- test_api.py
+++ test_api.py
@@ -1,1 +1,16 @@
-test_api.py
+## tests/test_api.py
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Carolina Bandwidth is live"}
+
+
+def test_text_endpoint():
+    response = client.post("/text", json={"input_text": "Hello"})
+    assert response.status_code == 200
+
